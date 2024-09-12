2ceb5eb2675d5697770c9e675e655bd253c0aa8a
Kevin R Pasato <kevinpas@buffalo.edu>
2024-09-12 18:05:28 -0400

    CSV_reader

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#define MAX_VAL 999
 int main(int argc, char * argv[]){
 
     printf("Hello world.\n");
 
     return 0;
 }
+
+void csv_reader(const char *filename){
+    File *csv_file = fopen(filename, "r");
+    char buffer[MAX_VAL]
+    while (fgets(line,MAX_VAL,csv_file)) {    
+
+    }
+    fclose(csv_file); 
+}
+
+ 
