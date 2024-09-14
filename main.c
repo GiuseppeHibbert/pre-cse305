0042bf10b18645881690571d94a77b5e837fe640
Kevin R Pasato <kevinpas@buffalo.edu>
2024-09-13 20:42:13 -0400

    Beginning some code

--- main.c
+++ main.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_VAL 999
-int main(int argc, char * argv[]){
+#define Line_max 999
+#define Field_max 999 
 
-    // load the csv file in for input.
 
-    
-    printf("Hello world.\n");
-
+int is_a_number(const char *string){//for max min mean fields
+    for(int i=0;string[i]!='\0';i++){// iterating through string
+        if(!isdigit(str[i]) && str[i] != '-' && str[i] !='.')
+            return 0;}
+    return 1;} // is a num if returns 0.
+double min_field(int field,int header, FILE *file){
     return 0;
 }
-
-void csv_reader(const char *filename){
-    File *csv_file = fopen(filename, "r");
-    char buffer[MAX_VAL]
-    while (fgets(line,MAX_VAL,csv_file)) {    
-
-    }
-    fclose(csv_file); 
+double max_field(int field,int header, FILE *file){
+    return 0;
 }
+double mean(int field,int header, FILE *file){
+    return 0;}
 
- 
+
+
+int main(int argc, char * argv[]){
+    // load the csv file in for input.
+    printf("Hello world.\n");
+    return 0;
+}
