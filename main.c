9b616dcf25c5f47d477b16486f466158cf081f02
Kevin R Pasato <kevinpas@buffalo.edu>
2024-09-16 16:48:35 -0400

    working on the max_field

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <float.h>
+#include <limits.h>
 #define Line_max 999
 #define Field_max 999 
 
@@ -10,9 +12,20 @@ int is_a_number(const char *string){//for max min mean fields
         if(!isdigit(str[i]) && str[i] != '-' && str[i] !='.')
             return 0;}
     return 1;} // is a num if returns 0.
-double min_field(int field,int header, FILE *file){
-    return 0;
-}
+double min_field(int field,int head_line, FILE *file){
+    double min = DBL_MAX;// using the max to get compare with the current fields number 
+    int first_line_field_val = 1;// first line
+    char line_on[Line_max];
+    rewind(file)
+        while( fgets(line_on,Line_max, file)){
+            if(first_line_field_val && head_line){
+                first_line_field_val=0;
+                continue;}// This skips the head line if its not
+            char *copying = strdup(line_on);
+            double = current_field = 0;
+            char * break_string; 
+                return 0;
+        }}
 double max_field(int field,int header, FILE *file){
     return 0;
 }
